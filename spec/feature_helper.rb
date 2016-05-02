1e42b1f2db708522671476a836b83ee4393db552
marinacor1 <marinaspam1234@gmail.com>
2016-05-02 09:36:26 -0500

    Creating feature helper method for user login

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -1,2 +1,12 @@
 module FeatureHelper
+
+  def user_login
+    visit login_path
+
+    fill_in "Username", with: "Marina"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    expect(current_path).to eq(user_dashboard_path)
+  end
 end
